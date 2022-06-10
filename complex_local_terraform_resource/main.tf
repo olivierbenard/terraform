1441287150e2231835710aea21ec85d71040c614
Olivier Bénard <olivier.benard@telecomnancy.net>
2022-06-10 18:36:58 +0200

    Add Makefile

--- complex_local_terraform_resource/main.tf
+++ complex_local_terraform_resource/main.tf
@@ -12,8 +12,8 @@ terraform {
 # it assigns a name to an expression you can use multiple time within
 # a module instead of repeating the expression
 locals {
-  folder = "outputs"
-  environment = "${terraform.workspace}"
+  folder      = "outputs"
+  environment = terraform.workspace
   required_tags = {
     project     = var.project_name,
     environment = var.environment
@@ -22,11 +22,10 @@ locals {
   name_prefix = "${var.project_name}-${local.environment}"
 }
 
-
 # by default a resource block configures one real infrastructure object
 # to create multiple similar instance, use `count`
 resource "local_file" "foo" {
-  count    = "${local.environment == "prod" ? 0 : local.environment == "staging" ? 2 : 4}" # meta-argument to create n similar .txt instances
+  count    = local.environment == "prod" ? 0 : local.environment == "staging" ? 2 : 4 # meta-argument to create n similar .txt instances
   filename = "${local.folder}/${local.name_prefix}-foo${count.index}.txt"
   content  = "foo!"
 }
